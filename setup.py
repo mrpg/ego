70a83712a3b25c259dd889e98aea157f41629b09
Max R. P. Grossmann <m@max.pm>
2023-12-06 10:54:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alter_ego",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "ego=ego:cli.main",  # Adjust if necessary
+        ],
+    },
+    install_requires=open("requirements.txt").read().splitlines(),
+    python_requires=">=3.8",
+    author="Max R. P. Grossmann",
+    author_email="ego@mg.sb",
+    description="Library to run experiments with LLMs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrpg/ego",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later",
+        "Operating System :: OS Independent",
+    ],
+)
